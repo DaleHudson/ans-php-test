80dcca99b3822cd01a9d98dcf0fb0c0b5e4232d8
Dale Hudson <dalehudson26@gmail.com>
2024-09-25 20:43:06 +0100

    [Views] Updated pokemon index view

--- ans-pokedex/resources/views/pokemon/index.blade.php
+++ ans-pokedex/resources/views/pokemon/index.blade.php
@@ -1,32 +1,40 @@
+@extends('layout.app')
+
+@section('content')
 <div>
     <div>
-        <form action="{{ route('pokemon.search') }}" method="POST">
+        <form action="{{ route('pokemon.search') }}" method="POST" id="searchForm">
             @csrf
-            <input type="text" name="name" placeholder="Search for a Pokemon">
+            <input type="text" name="name" id="name" placeholder="Search for a Pokemon">
             <button type="submit">Search</button>
         </form>
     </div>
     <div>
         @if(count($pokemon) > 0)
             <h1>Pokemon</h1>
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Url</th>
-                </tr>
-                @foreach($pokemon as $poke)
+            <table id="pokemonTable">
+                <thead>
                     <tr>
-                        <td>
-                            {{ $poke->getName() }}
-                        </td>
-                        <td>
-                            <a href="{{ $poke->getUrl() }}">More Info</a>
-                        </td>
+                        <th>Name</th>
+                        <th>Url</th>
                     </tr>
-                @endforeach
+                </thead>
+                <tbody>
+                    @foreach($pokemon as $poke)
+                        <tr>
+                            <td>
+                                {{ $poke->getName() }}
+                            </td>
+                            <td>
+                                <a href="{{ route('pokemon.show', $poke->getName()) }}">More Info</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
             </table>
         @else
             <h1>No Pokemon</h1>
         @endif
     </div>
 </div>
+@endsection
