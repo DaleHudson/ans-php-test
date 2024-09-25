dda181dad3f8a7c6ed3fe1426a20120501cddb66
Dale Hudson <dalehudson26@gmail.com>
2024-09-25 20:43:56 +0100

    [Resources] Added js to filter through list of pokemon

--- ans-pokedex/resources/js/app.js
+++ ans-pokedex/resources/js/app.js
@@ -1,1 +1,20 @@
 import './bootstrap';
+
+const searchForm = document.getElementById('searchForm');
+
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const token = document.querySelector('input[name="_token"]').value;
+
+    axios.post('/search', {
+        name: name,
+        _token: token
+    }).then(function (response) {
+        const tableBody = document.querySelector('#pokemonTable tbody');
+        tableBody.innerHTML = response.data;
+    }).catch(function (error) {
+        console.log(error);
+    });
+});
