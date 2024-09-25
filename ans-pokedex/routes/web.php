0fefd29caca69d4bbf35af6cdc3ce5b0164258a6
Dale Hudson <dalehudson26@gmail.com>
2024-09-25 20:44:28 +0100

    [Routes] Added pokemon show route

--- ans-pokedex/routes/web.php
+++ ans-pokedex/routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Http\Controllers\Pokemon\RetrieveAllPokemonController::class)->name('pokemon.index');
 Route::post('/search', \App\Http\Controllers\Pokemon\SearchPokemonController::class)->name('pokemon.search');
+Route::get('/show/{name}', \App\Http\Controllers\Pokemon\ShowPokemonController::class)->name('pokemon.show');
