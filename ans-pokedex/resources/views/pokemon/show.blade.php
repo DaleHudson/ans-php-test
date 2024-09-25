e14787a242ec91dce4b9dae9ed430721489b23cc
Dale Hudson <dalehudson26@gmail.com>
2024-09-25 21:16:52 +0100

    [Views] Updated pokemon show blade view

--- ans-pokedex/resources/views/pokemon/show.blade.php
+++ ans-pokedex/resources/views/pokemon/show.blade.php
@@ -1,5 +1,31 @@
 @extends('layout.app')
 
 @section('content')
-    <h1>Show</h1>
+    <a href="{{ route('pokemon.index') }}">All Pokemon</a>
+    
+    <h1>{{ $pokemon->getName() }}</h1>
+
+    <img src="{{ $pokemon->getImage() }}" alt="image of a pokemon" title="{{ $pokemon->getName() }}" />
+
+    <h3>Info</h3>
+    <ul>
+        <li>
+            <strong>Height:</strong> {{ $pokemon->getHeight() }}
+        </li>
+        <li>
+            <strong>Weight:</strong> {{ $pokemon->getWeight() }}
+        </li>
+        <li>
+            <strong>Type:</strong> {{ $pokemon->getType() }}
+        </li>
+    </ul>
+
+    <h3>Abilities</h3>
+    @if (count($pokemon->getAbilities()) > 0)
+        <ul>
+            @foreach($pokemon->getAbilities() as $ability)
+                <li>{{ $ability->getName() }}</li>
+            @endforeach
+        </ul>
+    @endif
 @endsection
