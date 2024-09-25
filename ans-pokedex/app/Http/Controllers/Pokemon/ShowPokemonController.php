769639adc9e1be793888971956fe5c7abcb3e28b
Dale Hudson <dalehudson26@gmail.com>
2024-09-25 21:16:08 +0100

    [Controllers] Updated show pokemon controller

--- ans-pokedex/app/Http/Controllers/Pokemon/ShowPokemonController.php
+++ ans-pokedex/app/Http/Controllers/Pokemon/ShowPokemonController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\Pokemon;
 
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
 class ShowPokemonController extends BasePokemonController
 {
-    public function __invoke()
+    public function __invoke(Request $request): View
     {
-        return view('pokemon.show');
+        try {
+            // TODO sanitize/validate input here
+            $pokemon = $this->pokemonApiService->getPokemonByName($request->name);
+        } catch (Exception $e) {
+            dd($e->getMessage());
+            // TODO display flash message here
+        }
+
+        return view('pokemon.show', compact('pokemon'));
     }
 }
