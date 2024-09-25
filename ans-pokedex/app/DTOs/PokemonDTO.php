eab15cf17ce7089743fc7aa53d294cf2d0296215
Dale Hudson <hudsonn90@gmail.com>
2024-09-25 18:24:03 +0100

    [DTO] Added initial dto

---
+++ ans-pokedex/app/DTOs/PokemonDTO.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DTOs;
+
+class PokemonDTO
+{
+    public function __construct(
+        private string $name,
+        private string $url
+    ) {}
+
+    public static function fromApiResponse(array $response): self
+    {
+        return new self(
+            $response['name'],
+            $response['url']
+        );
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+}
