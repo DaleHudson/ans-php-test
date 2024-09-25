7a7ddfd810da92aa9c849a53b4be93cd0946afc8
Dale Hudson <dalehudson26@gmail.com>
2024-09-25 21:22:31 +0100

    [Controllers] Cache results from all pokemon

--- ans-pokedex/app/Http/Controllers/Pokemon/RetrieveAllPokemonController.php
+++ ans-pokedex/app/Http/Controllers/Pokemon/RetrieveAllPokemonController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Services\PokemonApiService;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class RetrieveAllPokemonController extends BasePokemonController
@@ -14,7 +15,12 @@ class RetrieveAllPokemonController extends BasePokemonController
     {
         $pokemon = null;
         try {
-            $pokemon = $this->pokemonApiService->getAllPokemon();
+            if (!Cache::has("pokemon")) {
+                $pokemonData = $this->pokemonApiService->getAllPokemon();
+                Cache::put("pokemon", $pokemonData, 3600);
+            }
+
+            $pokemon = Cache::get("pokemon");
         } catch (Exception $e) {
             dd($e->getMessage());
             // TODO display flash message here
